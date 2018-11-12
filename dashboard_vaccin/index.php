9f1d6acacc5f10a139706e809f24938d652ffbbd
E-Tsuki <tsuki_chan@live.fr>
2018-11-12 12:11:58 +0100

    12112018 1211

--- dashboard_vaccin/index.php
+++ dashboard_vaccin/index.php
@@ -7,13 +7,30 @@ include('inc/sidebar.php');
  <section class="content-header">
    <h1>
      Home
-     <small></small>
    </h1>
    <ol class="breadcrumb">
      <li><a href="#"><i class="fa fa-dashboard"></i>Home</a></li>
    </ol>
  </section>
-    <!-- Main content -->
+
+ <div class="box box-info">
+       <div class="box-header with-border">
+         <h3 class="box-title">Nombre d'utilisateurs inscrits en 2018</h3>
+
+         <div class="box-tools pull-right">
+           <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+           </button>
+           <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+         </div>
+       </div>
+       <div class="box-body">
+         <div class="chart">
+           <canvas id="lineChart" style="height: 300px; width: 361px;" width="361" height="300"></canvas>
+         </div>
+       </div>
+       <!-- /.box-body -->
+  </div>
+
     <section class="content">
       <!-- Default box -->
         <div class="box-footer">
@@ -26,5 +43,7 @@ include('inc/sidebar.php');
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-  <?php
-  include('inc/footer.php');
+
+
+
+  <?php include('inc/footer.php');
