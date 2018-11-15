bc862f596ec81c86fb6c13a45946fb5be2fdf748
Kanarpp <lecarnetduvagabond@gmail.com>
2018-11-15 14:28:02 +0100

    15/11 1427

--- profil.php
+++ profil.php
@@ -39,7 +39,7 @@ if(is_logged()){
 
 // Requete des vaccins obligatoires
       $sql = "SELECT * FROM v5_vaccin AS v
-              LEFT JOIN v5_relation AS pivot
+              JOIN v5_relation AS pivot
               ON v.id = pivot.vaccin_id
               WHERE pivot.user_id = $id
               AND v.obligatoire = 1";
@@ -50,9 +50,7 @@ if(is_logged()){
       $tableauId = array();
       foreach ($vaccinObligatoires as $v) {
             $tableauId[] = $v['vaccin_id'];
-
       }
-
 
 
     }else {
@@ -77,6 +75,7 @@ include('inc/header.php'); ?>
               <li>  Sexe : <?php echo $profil['sexe'] ; ?> </li>
               <li>  mail :  <?php echo $profil['mail'] ; ?> </li>
               <li>  Date de Naissance : <?php echo $profil['date_naissance'] ;?></li>
+
         </ul>
         <div class="button_div">
           <a class="button" href="modifier_profil.php">Modifier profil</a>
@@ -84,59 +83,63 @@ include('inc/header.php'); ?>
     </div>
 </div>
 
-<div class="modif_vaccin">
-
-  <h2>Vaccins Obligatoires</h2>
-    <table class="form table_vaccin">
-
-      <tr>
-        <th class="parent"><p class="enfant">Nom</p></th>
-        <th class="parent"><p class="enfant">Fait</p></th>
-        <th class="parent"><p class="enfant">Fréquences d'injection</p></th>
-        <th class="parent"><p class="enfant">Rappel</p></th>
-      </tr>
-
-      <?php foreach ($listVaccinObligatoires as $listVaccinObligatoire):
-        ?> <tr> <?php
-        echo '<td class="parent"><p class="enfant">' .$listVaccinObligatoire['nom']. '</p></td>';
-
-          if(in_array($listVaccinObligatoire['id'],$tableauId)) {
-            echo '<td class="parent"><img class="enfant" src="assets/image/icon_fait.svg" alt="Fait"></td>';
-          } else {
-            echo '<td class="parent"><img class="enfant" src="assets/image/icon_non_fait.svg" alt="Fait"></td>';
-          }
-
-        echo '<td class="parent"><p class="enfant">' .$listVaccinObligatoire['frequences_injections']. '</p></td>';
-        echo '<td class="parent"><p class="enfant">' .$listVaccinObligatoire['frequences_injections']. '</p></td>';
-           ?> </tr> <?php
-          endforeach; ?>
-    </table>
-
-    <h2>Vaccins non obligatoires efféctués</h2>
-    <table class="form table_vaccin">
-
-      <tr>
-        <th class="parent"><p class="enfant">Nom</p></th>
-        <th class="parent"><p class="enfant">Fréquences d'injection</p></th>
-        <th class="parent"><p class="enfant">Rappel</p></th>
-      </tr>
-
-      <?php foreach ($vaccinNonObligatoires as $vaccinNonObligatoire):
-        ?> <tr> <?php
-            echo '<td class="parent"><p class="enfant">' .$vaccinNonObligatoire['nom']. '</p></td>';
-            echo '<td class="parent"><p class="enfant">' .$vaccinNonObligatoire['frequences_injections']. '</p></td>';
-            echo '<td class="parent"><p class="enfant">' .$vaccinNonObligatoire['rappel']. '</p></td>';
-       ?> </tr> <?php
-      endforeach; ?>
-
-    </table>
-
-    <div class="button_div">
-      <a class="button center" href="modif_vaccin.php">Ajout retrait de vaccin</a>
-    </div>
-
-    <div class="button_div">
-      <a class="button" href="index.php">Retour</a>
-    </div>
+<div class="button_div">
+  <a class="button center" href="modif_vaccin.php">Ajout retrait de vaccin</a>
 </div>
+
+<?php if ($vaccin_id = $tableauId) {
+  ?><div class="modif_vaccin">
+
+    <h2>Vaccins obligatoires efféctués</h2>
+      <table class="form table_vaccin">
+
+        <tr>
+          <th class="parent"><p class="enfant">Nom</p></th>
+          <th class="parent"><p class="enfant">Fréquences d'injection</p></th>
+          <th class="parent"><p class="enfant">Rappel</p></th>
+        </tr>
+
+        <?php foreach ($vaccinObligatoires as $vaccinObligatoire):
+          ?> <tr> <?php
+          echo '<td class="parent"><p class="enfant">' .$vaccinObligatoire['nom']. '</p></td>';
+
+            // if(in_array($vaccinObligatoire['id'],$tableauId)) {
+            //   echo '<td class="parent"><img class="enfant" src="assets/image/icon_fait.svg" alt="Fait"></td>';
+            // } else {
+            //   echo '<td class="parent"><img class="enfant" src="assets/image/icon_non_fait.svg" alt="Fait"></td>';
+            // }
+
+          echo '<td class="parent"><p class="enfant">' .$vaccinObligatoire['frequences_injections']. '</p></td>';
+          echo '<td class="parent"><p class="enfant">' .$vaccinObligatoire['rappel']. '</p></td>';
+             ?> </tr> <?php
+            endforeach; ?>
+      </table>
+
+      <h2>Vaccins non obligatoires efféctués</h2>
+      <table class="form table_vaccin">
+
+        <tr>
+          <th class="parent"><p class="enfant">Nom</p></th>
+          <th class="parent"><p class="enfant">Fréquences d'injection</p></th>
+          <th class="parent"><p class="enfant">Rappel</p></th>
+        </tr>
+
+        <?php foreach ($vaccinNonObligatoires as $vaccinNonObligatoire):
+          ?> <tr> <?php
+              echo '<td class="parent"><p class="enfant">' .$vaccinNonObligatoire['nom']. '</p></td>';
+              echo '<td class="parent"><p class="enfant">' .$vaccinNonObligatoire['frequences_injections']. '</p></td>';
+              echo '<td class="parent"><p class="enfant">' .$vaccinNonObligatoire['rappel']. '</p></td>';
+         ?> </tr> <?php
+        endforeach; ?>
+
+      </table>
+
+      <div class="button_div">
+        <a class="button" href="index.php">Retour</a>
+      </div>
+  </div><?php
+}else {
+
+} ?>
+
 <?php include('inc/footer.php');
