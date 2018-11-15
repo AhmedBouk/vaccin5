9ed3d1b9fee430058ff0a262ec75f176511b4267
camillelupo <camille.lupo@hotmail.fr>
2018-11-15 14:05:16 +0100

    15/11/2018 1405

--- 404.php
+++ 404.php
@@ -10,8 +10,8 @@ include('inc/header.php') ?>
 				<h3 id="fittext1">Oops! Page non trouvée.</h3>
 				<h2 id="fittext2"><span>4</span><span>0</span><span>4</span></h2>
 			</div>
-			<h4 id="fittext1">Nous sommes désolé, nous ne trouvons pas la page.</h4>
-			<p><a href="index.php">Cliquez ici</a> pour revenir sur notre site.</p>
+			<h4 id="fittext3">Nous sommes désolé, nous ne trouvons pas la page.</h4>
+			<p id="fittext4"><a href="index.php">Cliquez ici</a> pour revenir sur notre site.</p>
 		</div>
 	</div>
 
