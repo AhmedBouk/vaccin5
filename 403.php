3b69a80c47876e24041cd2b417c5c3d5470d77e7
E-Tsuki <tsuki_chan@live.fr>
2018-11-15 10:39:30 +0100

    15112018 1039

--- 403.php
+++ 403.php
@@ -10,7 +10,7 @@ include('inc/header.php') ?>
 				<h3 id="fittext1">Oops! Page non trouvée</h3>
 				<h2 id="fittext1"><span>4</span><span>0</span><span>3</span></h2>
 			</div>
-			<h4 id="fittext1">Nous sommes désolé, vous n'avez pas les droits pour accéder à cette page</h4>
+			<h4 id="fittext1">Nous sommes désolé, vous n'avez pas les droits pour accéder à cette page, cliquez <a href="#">ici</a> pour revenir sur notre site !</h4>
 		</div>
 	</div>
 
